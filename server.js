689f6a3f81902faea9d8716cb5672452b875454b
André <Andr@ANDRE-PC>
2018-11-13 21:41:12 -0200

    Setup start script

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-
+const port = process.env.port || 3000;
 var app = express();
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
@@ -34,4 +34,6 @@ app.get('/about', (req, res) => {
     });
 });
 
-app.listen(3000);
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
